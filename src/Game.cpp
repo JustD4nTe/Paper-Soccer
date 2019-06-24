5e10117b8007c564abfa3e3f9f5dd11f24d603de
ups <upsu90@gmail.com>
2019-06-24 09:48:22 +0200

    fixing some bugs

--- src/Game.cpp
+++ src/Game.cpp
@@ -15,7 +15,7 @@ void Game::draw(sf::RenderWindow& mWindow,sf::Texture& m_texture) {
 		//display error about missing file
 	}
 	
-	mWindo.setTexture(m_texture);
+	m_sprite.setTexture(m_texture);
 	m_sprite.setTextureRect(sf::IntRect(0, 0, 1024, 800));
 	mWindow.draw(m_sprite);
 	m_board.drawBoard(&mWindow);
